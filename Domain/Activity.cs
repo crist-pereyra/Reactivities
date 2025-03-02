2b0ae89dff3d24d07282de3c50c3c9ad4a0a31b5
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2025-03-02 17:13:30 -0400

    Create DbContext with Entity Framework

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    class Activity
+    public class Activity
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public required string Title { get; set; }
