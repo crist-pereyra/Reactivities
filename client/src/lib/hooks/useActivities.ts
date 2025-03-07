39eae49d2825e92b31912b9fd1bf6eb07cb2efd8
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2025-03-06 20:25:13 -0400

    Add routes

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -2,18 +2,23 @@ import activityApi from '@/lib/api/activity.api';
 import { Activity } from '@/lib/interfaces/activity';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import { useActivityStore } from '../stores/activity.store';
 
-export const useActivities = () => {
-  const setActivities = useActivityStore((state) => state.setActivities);
+export const useActivities = (id?: string) => {
   const queryClient = useQueryClient();
   const { data: activities, isPending } = useQuery({
     queryKey: ['activities'],
     queryFn: async () => {
       const { data } = await activityApi.get<Activity[]>('/activities');
-      setActivities(data);
       return data;
     },
+  });
+  const { data: activity, isLoading: isActivityLoading } = useQuery({
+    queryKey: ['activity', id],
+    queryFn: async () => {
+      const { data } = await activityApi.get<Activity>(`/activities/${id}`);
+      return data;
+    },
+    enabled: !!id,
   });
 
   const updateActivity = useMutation({
@@ -60,5 +65,7 @@ export const useActivities = () => {
     updateActivity,
     createActivity,
     deleteActivity,
+    activity,
+    isActivityLoading,
   };
 };
